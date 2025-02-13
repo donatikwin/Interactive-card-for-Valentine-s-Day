28922173508a0fb4f2618be8da961b176b388a20
Danil Donatikov <140803799+donatikwin@users.noreply.github.com>
2025-02-14 00:49:21 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const envelope = document.querySelector('.envelope-wrapper');
+let isAnimating = false;
+
+envelope.addEventListener('click', () => {
+    if (isAnimating) return;
+    
+    envelope.classList.add('flap');
+    isAnimating = true;
+
+    setTimeout(() => {
+        isAnimating = false;
+        envelope.style.pointerEvents = 'none';
+
+        const button = document.createElement('a');
+        button.classList.add('next');
+        button.textContent = 'Нажми сюда';
+
+        document.querySelector('.letter .text').appendChild(button);
+
+        setTimeout(() => {
+            button.classList.add('fade-in');
+        }, 100);
+
+        button.addEventListener('click', () => {
+            window.location.href = '/new.html';
+        });
+    }, 1700); 
+});
+
